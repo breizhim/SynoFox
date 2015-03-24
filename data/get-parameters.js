9dd1ae3d865cf277514b4d026cbfc3dcc715e23f
breizhim <breizhim@gmail.com>
2015-03-24 23:21:10 +0100

    Parameters panel
    Display: ok, get_values: ok !

--- data/get-parameters.js
+++ data/get-parameters.js
@@ -2,9 +2,11 @@
 // message to main.js.
 // The message payload is the contents of the edit box.
 var button = document.getElementById("button");
-button.addEventListener('onclick', function onclick(event) {
-    self.port.emit("text-entered", text);
-    textArea.value = '';
+button.addEventListener('click', function onclick(event) {
+	ip = document.getElementById("address").value;
+	username = document.getElementById("username").value;
+	password = document.getElementById("password").value;
+    self.port.emit("text-entered", ip,username,password);
 }, false);
 // Listen for the "show" event being sent from the
 // main add-on code. It means that the panel's about
