342e536c1c3ac58d78349baf4dac72be5f5f1d89
breizhim <breizhim@gmail.com>
2015-03-25 16:05:19 +0100

    Debut intégration des paramètres

--- data/get-parameters.js
+++ data/get-parameters.js
@@ -6,7 +6,8 @@ button.addEventListener('click', function onclick(event) {
 	ip = document.getElementById("address").value;
 	username = document.getElementById("username").value;
 	password = document.getElementById("password").value;
-    self.port.emit("text-entered", ip,username,password);
+	testConnect(ip,username,password);
+    //self.port.emit("text-entered", ip,username,password);
 }, false);
 // Listen for the "show" event being sent from the
 // main add-on code. It means that the panel's about
@@ -17,3 +18,16 @@ button.addEventListener('click', function onclick(event) {
 self.port.on("show", function onShow() {
   document.getElementById("address").focus();
 });
+
+function testConnect(ip,username,password) {
+	var Request = require("sdk/request").Request;
+	var login = Request({
+	url: "http://"+ip+":5000/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account="+username+"&passwd="+password+"&session=DownloadStation&format=sid",
+	onComplete: function (response) {
+		var myconnect = response.json;
+		console.log("http://"+ip+":5000/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account="+username+"&passwd="+password+"&session=DownloadStation&format=sid");
+		console.log("Connection: " + myconnect.success);
+	}
+});
+login.get();
+}
